cdeece618b82d309ece1c1d300bd20cf7c1de283
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-04-24 15:39:32 +0300

    Added Client model with to_dict method

---
+++ models/client.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    programs = db.relationship('Program', secondary='client_program', backref='clients')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'phone': self.phone,
+            'programs': [program.name for program in self.programs]
+        }
+
+client_program = db.Table('client_program',
+    db.Column('client_id', db.Integer, db.ForeignKey('client.id'), primary_key=True),
+    db.Column('program_id', db.Integer, db.ForeignKey('program.id'), primary_key=True)
+)
