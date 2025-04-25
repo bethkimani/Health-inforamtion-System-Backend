07742524eb640aaf4bea2ccfe8a2dfeca99eba36
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-04-25 12:39:05 +0300

    Add config.py, reinitialize migrations

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '6195a52892f58c880a90916f0c5f6102')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///health_system.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///instance/health_system.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', '9ae9a72f2ca05a7b1b798e30306a634')
